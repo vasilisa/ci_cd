d4793daf02cac5028fe459d264848fed941130e4
admin <admin>
2024-12-11 16:00:14 +0000

    Git references update for MLOPSDEMO

--- lib/python/CI-CD/hello_dss.py
+++ lib/python/CI-CD/hello_dss.py
@@ -1,3 +1,3 @@
 def say_hello():
-    user_name = input("Hey, what's your name?")
+    user_name = input("Hey, what's your name? How are you today?")
     print(f'Hello from DSS PLAYGROUND {user_name}! How are you today? Ciao!')
