7f8c894f53888ade9a46558828f5654940fb90e7
admin <admin>
2025-02-10 19:07:52 +0000

    Edited Library file "python/CI-CD/hello_dss.py"

--- lib/python/CI-CD/hello_dss.py
+++ lib/python/CI-CD/hello_dss.py
@@ -1,3 +1,3 @@
 def say_hello():
     user_name = input("Hey, what's your name? How are you today?\n")
-    print(f'Hello from DSS PLAYGROUND {user_name}!\n How are you today? Ciao!')
+    print(f'Hello from DSS PLAYGROUND {user_name}! How are you today?\n Ciao!')
