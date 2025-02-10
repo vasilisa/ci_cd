19ebf745d77c694e6afe38486590ad99c21d1394
admin <admin>
2025-02-10 19:43:32 +0000

    Git references update for MLOPSDEMO

--- lib/python/custom.py
+++
@@ -1,9 +0,0 @@
-from sklearn.isotonic import IsotonicRegression
-
-class IsotonicWrapper(IsotonicRegression): 
-    def fit(self,X,y,sample_weight = None):
-        super().fit(X.ravel,y,sample_weight=sample_weight)
-        
-    def predict(self,T):
-        return super().predict(T.ravel())
-
